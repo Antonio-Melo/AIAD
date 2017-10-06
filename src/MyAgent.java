e7efd794e89c69ca499ac99e4c8ff8e2001c2150
Antonio-Melo <happyd3nt@gmail.com>
2017-10-06 20:26:27 +0100

    Testes

---
+++ src/MyAgent.java
@@ -0,0 +1,14 @@
+/**
+ * Created by amelo on 9/27/17.
+ */
+public class MyAgent extends jade.core.Agent
+{
+    public void setup(){
+        System.out.println("Hello World!");
+    }
+
+    public void takeDown(){
+        System.out.println("Bye...");
+    }
+}
+
