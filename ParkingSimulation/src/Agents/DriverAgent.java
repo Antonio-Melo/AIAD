2f50b5a201da60b6a49ef94f7722217b99553d0c
Pedro Costa <pedrodc23@gmail.com>
2017-11-08 13:23:20 +0000

    Starting to figure this out

---
+++ ParkingSimulation/src/Agents/DriverAgent.java
@@ -0,0 +1,107 @@
+package Agents;
+
+
+import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.space.SpatialMath;
+import  repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.NdPoint;
+import  repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridPoint;
+import sajas.core.Agent;
+
+public class DriverAgent extends Agent{
+    
+	private static final long serialVersionUID = 1L;
+	private static int IDNumber = 0;
+	private int ID;
+    private int startX;
+    private int startY;
+    private int destinationX;
+    private int destinationY;
+    private int arrival;
+    private float maxPricePerHour;
+    private int durationOfStay;
+    private int maxWalkingDistance;
+    private int initialTime;
+    private int day;
+    private ContinuousSpace<Object> space;
+    private Grid<Object> grid;
+    private boolean moved;
+    
+    public DriverAgent(ContinuousSpace <Object > space , Grid <Object > grid, int startX, int startY, int destinationX, int destinatioY, int arrival, float maxPricePerHour, int durationOfStay, int maxWalkingDistance, int initialTime, int day) {
+    	
+    	IDNumber++;
+		ID = IDNumber;
+        this.startX = startX;
+        this.startY = startY;
+        this.destinationX = destinationX;
+        this.destinationY = destinatioY;
+        this.arrival = arrival;
+        this.maxPricePerHour = maxPricePerHour;
+        this.durationOfStay = durationOfStay;
+        this.maxWalkingDistance = maxWalkingDistance;
+        this.initialTime = initialTime;
+        this.day = day;
+        this.space = space;
+        this.grid = grid;
+    }
+    
+    public  void  moveTowards(GridPoint  pt) {
+    	
+    	// only  move if we are  not  already  in this  grid  location
+    	if (!pt.equals(grid.getLocation(this))) {
+	    	NdPoint  myPoint = space.getLocation(this);
+	    	NdPoint  otherPoint = new  NdPoint(pt.getX(), pt.getY ());
+	    	double  angle = SpatialMath.calcAngleFor2DMovement(space ,	myPoint , otherPoint );
+	    	space.moveByVector(this , 1, angle , 0);
+	    	myPoint = space.getLocation(this);
+	    	grid.moveTo(this , (int)myPoint.getX(), (int)myPoint.getY ());
+	    	
+	    	moved = true;
+    	}
+	}
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getStartX() {
+        return startX;
+    }
+
+    public int getStartY() {
+        return startY;
+    }
+
+    public int getDestinationX() {
+        return destinationX;
+    }
+
+    public int getDestinationY() {
+        return destinationY;
+    }
+
+    public int getArrival() {
+        return arrival;
+    }
+
+    public float getMaxPricePerHour() {
+        return maxPricePerHour;
+    }
+
+    public int getDurationOfStay() {
+        return durationOfStay;
+    }
+
+    public int getMaxWalkingDistance() {
+        return maxWalkingDistance;
+    }
+
+    public int getInitialTime() {
+        return initialTime;
+    }
+
+    public int getDay() {
+        return day;
+    }
+}
---
+++ ParkingSimulation/src/Launcher/ParkingSimulationLauncher.java
@@ -0,0 +1,84 @@
+package Launcher;
+
+import Agents.DriverAgent;
+import jade.core.AID;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.wrapper.StaleProxyException;
+import repast.simphony.context.Context;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactory;
+import repast.simphony.context.space.continuous.ContinuousSpaceFactoryFinder;
+import repast.simphony.context.space.graph.NetworkBuilder;
+import repast.simphony.context.space.grid.GridFactory;
+import repast.simphony.context.space.grid.GridFactoryFinder;
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.continuous.RandomCartesianAdder;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridBuilderParameters;
+import repast.simphony.space.grid.SimpleGridAdder;
+import sajas.core.Agent;
+import sajas.core.Runtime;
+import sajas.sim.repasts.RepastSLauncher;
+import sajas.wrapper.ContainerController;
+import repast.simphony.space.grid.WrapAroundBorders;
+
+public class ParkingSimulationLauncher extends RepastSLauncher {
+
+	//Number of drivers to deploy
+	private static int N_DRIVERS = 10;
+	
+	private ContainerController agentContainer;	
+	private ContinuousSpace <Object > space;
+	private Grid <Object > grid;
+
+	public static void main(String[] args) {
+		ParkingSimulationLauncher model = new ParkingSimulationLauncher();	
+	}
+	
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected void launchJADE() {
+
+		Runtime rt = Runtime.instance();
+		Profile p1 = new ProfileImpl();
+		agentContainer = rt.createAgentContainer(p1);
+		
+		launchAgents();
+		
+	}
+	
+	private void launchAgents() {
+		
+		for(int i = 0; i < N_DRIVERS; i++){
+			
+			//TODO: Set Agents properties
+			DriverAgent a = new DriverAgent(space, grid, i, i, i, i, i, i, i, i, i, i);
+
+			try {
+				agentContainer.acceptNewAgent("Driver " + a.getID(), a).start();
+			} catch (StaleProxyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}		
+	}
+	
+	@Override
+	public Context build(Context<Object> context) {
+		// http://repast.sourceforge.net/docs/RepastJavaGettingStarted.pdf
+		
+		ContinuousSpaceFactory spaceFactory = ContinuousSpaceFactoryFinder.createContinuousSpaceFactory(null);
+		ContinuousSpace<Object> space = spaceFactory.createContinuousSpace("space", context, new RandomCartesianAdder <Object >(), new repast.simphony.space.continuous.WrapAroundBorders (), 50, 50);
+		GridFactory gridFactory = GridFactoryFinder.createGridFactory(null);
+		
+		Grid<Object> grid = gridFactory.createGrid("grid", context, new  GridBuilderParameters <Object >(new  WrapAroundBorders (),	new  SimpleGridAdder <Object >(), true , 50, 50));
+		
+		return super.build(context);
+	}
+
+}
