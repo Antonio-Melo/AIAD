6a45fe8228900e91f8d814e28e97e062bd2fe82b
Antonio-Melo <antonio.aptmelo@gmail.com>
2017-12-09 16:31:43 +0000

    correct initializers

--- ParkingSimulation/src/Agents/ParkingFacilityAgent.java
+++ ParkingSimulation/src/Agents/ParkingFacilityAgent.java
@@ -120,8 +120,8 @@ public class ParkingFacilityAgent extends Agent {
 	private Grid<Object> grid;
 	private ContinuousSpace<Object> space;
 
-	public ParkingFacilityAgent(ContinuousSpace<Object> space, Grid<Object> grid, ArrayList<Double> priceSchema, String name, String operator, int x,
-			int y, int capacity, double priceHour, double maxPrice, double minPrice, boolean dynamic, boolean lastUpdateDecision,double learningRate, double capacityDiscount) {
+	public ParkingFacilityAgent(ContinuousSpace<Object> space, Grid<Object> grid, String name, String operator, int x,
+			int y, int capacity, ArrayList<Double> priceSchema, double maxPrice, double minPrice, boolean dynamic, boolean lastUpdateDecision,double learningRate, double capacityDiscount) {
 		
 		this.numCars = 0;
 		this.deflationPrice = 1;
